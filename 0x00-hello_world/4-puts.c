47804bc61c39c16539500a032fb058b245f88169
Androzz23 <andres.novoa0@hotmail.com>
2021-06-18 00:22:21 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main (void){
-  puts("Programming is like building a miltilingual puzzle");
-
-    return (0);
+int main(void)
+{
+puts("Programming is like building a miltilingual puzzle");
+return (0);
 }
