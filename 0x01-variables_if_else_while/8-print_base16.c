e2032048933da5deecf60f7844fbc72179e622aa
Androzz23 <andres.novoa0@hotmail.com>
2021-06-19 23:42:42 -0700

    new file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+int i = 48;
+char c = 'a';
+
+while (i < 58 )
+{
+putchar(i);
+i++;
+}
+while (c < 'g')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
