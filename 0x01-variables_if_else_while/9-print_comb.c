71788b9b80cc6dd145c86b33fbcaaae9f58f5d1a
Androzz23 <andres.novoa0@hotmail.com>
2021-06-20 00:14:19 -0700

    new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 48;
+while (i < 58)
+{
+putchar(i);
+if (i == 57)
+{
+i++;
+}
+else
+{
+putchar(44);
+putchar(32);
+i++;
+}
+}
+putchar('\n');
+return (0);
+}
+
