b166cfb8c6eca8101772e00d1813bf228397be3a
Androzz23 <andres.novoa0@hotmail.com>
2021-06-19 21:39:22 -0700

    new document

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+char l = 'a';
+
+while (l <= 'z')
+{
+if ((l != 'e') && (l != 'q'))
+{
+putchar(l);
+l++;
+
+}
+else
+
+l++;
+}
+puthcar('\n')
+return (0); 
+}
