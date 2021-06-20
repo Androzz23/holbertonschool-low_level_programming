8cf7ba25a1778c75b64393b0c237d7afa7156c46
Androzz23 <andres.novoa0@hotmail.com>
2021-06-19 20:48:01 -0700

    .c that print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+char l = 'a';
+char f = 'z';
+
+while (l <= f)
+{
+putchar(l);
+l++;
+}
+
+ return (0);
+}
